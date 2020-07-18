46cca9eb22162da4811d83d8db6976d8f34a454d
Jorge <44483966+toweringdoor@users.noreply.github.com>
2020-07-17 20:16:49 -0500

    Removed some commands that weren't necessary since they were built into Red.

--- jorge/core.py
+++ jorge/core.py
@@ -16,43 +16,16 @@ class JorgeSetup(BaseCog):
         self.bot = bot
 
     @commands.command()
-    async def hello(self, ctx):
-        await ctx.send(
-            "Hey! What can I help you with? (If you need to ask any questions about this bot, please DM my creator ToweringDoor#3668)"
-        )
-
-    @commands.command()
-    async def genesis(self, ctx):
-        await ctx.send("gen is a big bot at rl")
-        print("Gen is pretty bad ngl")
-
-    @commands.command()
-    async def test(self, ctx):
-        await ctx.send("testing.... 1...2...3")
-        print("testcomplete")
-
-    @commands.command()
-    async def whyamimakingallthesecommands(self, ctx):
-        await ctx.send("i don't even know")
-        print("?")
-
-    @commands.command()
-    async def connection(self, ctx):
-        await ctx.send("I am successfully connected to Discord")
-        print("Discord connect successful")
+    async def bitcoin(self, ctx):
+        url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+        raw_response = requests.get(url)
+        response = raw_response.text
+        response = json.loads(response)
+        await ctx.send("Bitcoin price is: $" + response['bpi']['USD']['rate'])
 
     @commands.command()
     async def dj(self, ctx):
-        embed = discord.Embed(color=0x000000)
-        embed.set_image(url="https://cdn.discordapp.com/emojis/606483938176925697.png?v=1")
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    async def xavier(self, ctx):
-        await ctx.send(
-            "XAVIER = TRYHARD XAVIER = TRYHARD XAVIER = TRYHARD https://youtu.be/mRqOu150eFs"
-        )
-        print("XavierDONE")
+        await ctx.send("<:dj:730289249122910248>")
 
     @commands.command()
     async def usinfo(self, ctx, user: discord.Member):
@@ -73,25 +46,17 @@ class JorgeSetup(BaseCog):
     async def jacob(self, ctx):
         embed = discord.Embed(color=0x000000)
         embed.set_image(
-            url="https://cdn.discordapp.com/attachments/538926565774458890/637785286482460672/unknown.png"
+            url="https://cdn.discordapp.com/attachments/661670376744550416/730246288896950334/angry_pog.jpg"
         )
         await ctx.send(embed=embed, content="jacob is mega dumb")
 
     @commands.command()
-    async def doinyamom(self, ctx):
-        await ctx.send("doin ya mom doin doin ya mom")
-
-    @commands.command()
-    async def pig(self, ctx):
-        await ctx.send(":bacon:")
-
-    @commands.command()
-    async def joemama(self, ctx):
-        await ctx.send("no u")
-
-    @commands.command()
-    async def bruh(self, ctx):
-        await ctx.send(":loud_sound: bruh")
+    async def putinwalk(self, ctx):
+        embed = discord.Embed(color=0x000000)
+        embed.set_image(
+            url="https://media.discordapp.net/attachments/620444581871353868/733571027929399326/8680_PutinWideWalking.gif"
+        )
+        await ctx.send(embed=embed, content="putin do be walking tho :flushed:")
 
     @commands.command()
     async def guildinfo(self, ctx): # Had to rename, since serverinfo is already a command
@@ -103,63 +68,3 @@ class JorgeSetup(BaseCog):
         embed.add_field(name="Members", value=len(ctx.guild.members), inline=True)
         embed.set_thumbnail(url=ctx.guild.icon_url)
         await ctx.send(embed=embed)
-
-    @commands.command()
-    async def secret1(self, ctx):
-        await ctx.send("https://gyazo.com/96f189b524ad81c63d9be2882594e24d")
-
-    @commands.command()
-    async def lenny(self, ctx):
-        await ctx.send("( ͡° ͜ʖ ͡°)")
-
-    @commands.command()
-    async def uflip(self, ctx, user: discord.Member): #rename due to already use of command. u
-        await ctx.send("(╯°□°）╯︵ {}".format(user.name))
-        print("flipDONE")
-
-    @commands.command()
-    async def lex(self, ctx):
-        await ctx.send("lex is mega gamer")
-
-    @commands.command()
-    async def hoesmad(self, ctx):
-        await ctx.send("https://cdn.discordapp.com/attachments/540342714785464341/626531703648944138/hoesmad.jpg")
-
-    @commands.command()
-    async def invitelink(self, ctx):
-        await ctx.send("https://discordapp.com/oauth2/authorize?&client_id=592543480547639299&scope=bot&permissions=470019135")
-        print("LinkDONE")
-
-    @commands.command()
-    async def bitcoin(self, ctx):
-        url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
-        raw_response = requests.get(url)
-        response = raw_response.text
-        response = json.loads(response)
-        await ctx.send("Bitcoin price is: $" + response['bpi']['USD']['rate'])
-
-    @commands.command()
-    async def list_servers(self, ctx):
-        await ctx.send("This command does not send the server list to the chat. Instead, it sends the list to my creator or someone who has access to the bot.py file.")
-        print("Current Servers:")
-        for server in ctx.bot.guilds:
-            print(server.name)
-
-    @commands.command()
-    async def cheeseburger(self, ctx):
-        await ctx.send("Here ya go! :hamburger:")
-        print("BurgerDone")
-
-    @commands.command()
-    async def vibecheck(self, ctx):
-        await ctx.send("https://www.youtube.com/watch?v=PLOPygVcaVE&t")
-        print("vibeDONE")
-
-    @commands.command()
-    async def cheeseburger2(self, ctx):
-        await ctx.send("https://giphy.com/gifs/mcdonalds-cheeseburger-11KjGKVlosnuw0")
-        print("Burger2Done")
-
-    @commands.command()
-    async def genesis2(self, ctx):
-        await ctx.send("yea i agree")
